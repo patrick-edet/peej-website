ae000318d94bb699177e2c7b4a21601055240b8e
PEEJ <patrickedetjnr@outlook.com>
2025-08-06 10:29:31 +0200

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
   const hamburger = document.getElementById('hamburger');
   const navMenu = document.getElementById('navMenu');
 
-  hamburger.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-  });
-
-  // Toggle dropdown on mobile click
-  const menuItems = document.querySelectorAll('.navbar li');
-
-  menuItems.forEach(item => {
-    item.addEventListener('click', () => {
-      if (window.innerWidth <= 768) {
-        item.classList.toggle('open');
-      }
+  if (hamburger && navMenu) {
+    hamburger.addEventListener('click', () => {
+      navMenu.classList.toggle('active');
     });
-  });
 
+    const menuItems = document.querySelectorAll('.navbar li');
+    menuItems.forEach(item => {
+      item.addEventListener('click', () => {
+        if (window.innerWidth <= 768) {
+          item.classList.toggle('open');
+        }
+      });
+    });
+  }
+});
